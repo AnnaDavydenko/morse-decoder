d8e70f161f5a0134a68d24bd34987f3e36461c2c
AnnaDavydenko <annadavydenko2544737@gmail.com>
2020-10-08 23:05:50 +0300

    morse

--- src/index.js
+++ src/index.js
@@ -1,44 +1,77 @@
+const {
+    strikethrough
+} = require("colors");
+
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
+const isSpace = (sequance) => {
+    return sequance === "**********";
+}
+
+// .- -> a
+const decodeChar = (encoddedChar) => {
+    return MORSE_TABLE[encoddedChar];
+}
+
+// 1011 -> .-
+const normolizeAndDecodeSequance = (sequance) => {
+    let res = [];
+    for (let j = 0; j < sequance.length;) {
+        res.push(`${sequance[j]}${sequance[j+1]}`);
+        j += 2;
+    }
+    return res.filter(item => item !== "00").map(item => item === "10" ? "." : "-").join("");
+}
+
+
+const split10 = (str) => {
+    return str.match(/.{1,10}/g);
+}
+
 function decode(expr) {
-    // write your solution here
+    let string = "";
+    const splittedBy10 = split10(expr);
+    splittedBy10.forEach(sequance => {
+        string += isSpace(sequance) ? " " : decodeChar(normolizeAndDecodeSequance(sequance));
+    });
+    return string;
 }
 
 module.exports = {
